3b6fc1b75f0ae6132cfc289c52544b3d2b7a1d20
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-11-01 01:36:36 -0300

    add comments

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -19,6 +19,9 @@ function maxChar(str) {
   }
 
   for (let char in charMap) {
+    // charMap[char] is the key {1: a, 1: b, 7: c, 1: d}
+    // max = 0; loop over max 0 and updated max to 1: a and maxChar to char a, then again over 1: b (don't do anything)
+    // then over charMap[char] 7 that is great than max, so will save max to 7 and maxChar to c.
     if (charMap[char] > max) {
       max = charMap[char];
       maxChar = char;
