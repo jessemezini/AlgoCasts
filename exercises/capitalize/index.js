4d75ecd888e9c73e8e56a7d7c0475833a76e611e
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-11-08 11:10:41 -0200

    Ok

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -18,3 +18,15 @@ function capitalize(str) {
 }
 
 module.exports = capitalize;
+
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
