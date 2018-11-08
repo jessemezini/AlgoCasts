191937e971de43a24c209eb1e58999ffbf6388fe
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-11-08 11:10:10 -0200

    Ok

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const arr = [];
+
+  for (let word of str.split(' ')) {
+    arr.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return arr.join(' ');
+}
 
 module.exports = capitalize;
