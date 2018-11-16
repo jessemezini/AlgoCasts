0338d5aaf7c7be93397dcbf2e1d33d6dd8ead417
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-11-16 00:38:36 -0200

    Ok

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,32 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+
+  // let total = 0;
+
+  // Solution 2
+  // const wordsToCheck = ['a', 'e', 'i', 'o', 'u'];
+
+  // for (let char of str.toLowerCase()) {
+  //   if (wordsToCheck.includes(char)) {
+  //     total++;
+  //   }
+  // }
+
+  // Solution 1
+  // str
+  //   .toLowerCase()
+  //   .split('')
+  //   .filter(x => {
+  //     if (x === 'a' || x === 'e' || x === 'i' || x === 'o' || x === 'u') {
+  //       return total++;
+  //     }
+  //   });
+
+  // return total;
+}
 
 module.exports = vowels;
